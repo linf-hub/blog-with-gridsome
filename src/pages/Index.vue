ed9c5e68a02cae9a7f2c080838bfedc8806babf3
sth <fbyxth@163.com>
2021-08-02 00:54:24 +0800

    修改配置

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,7 +3,7 @@
     
     <!-- Page Header-->
     <header class="masthead" :style="{
-      backgroundImage: `url(${GRIDSOME_API_URL + general.cover[0].url})`
+      backgroundImage: `url(${GRIDSOME_API_URL + general.cover.url})`
     }">
         <div class="container position-relative px-4 px-lg-5">
             <div class="row gx-4 gx-lg-5 justify-content-center">
